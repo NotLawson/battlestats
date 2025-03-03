45816eebdb63e2ba00afc179f9a67af524a030ac
Lawson C <lawson.conallin@gmail.com>
2025-03-03 10:34:02 +1000

    pass int and json to the template

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def player_info(username):
     if user==None:
         return render_template('player_notfound.html', title="BS: Player Not Found")
     main_queue.put({"type":"sync", "username":username})
-    return render_template('player.html', user=user, title="BS: Player")
+    return render_template('player.html', user=user, title="BS: Player", int=int, json=json)
 
 
 
