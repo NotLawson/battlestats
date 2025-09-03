35b6be05a32c742f66afbaa30f44905c5d5c64ca
Lawson C <lawson.conallin@gmail.com>
2025-09-03 12:18:58 +1000

    add discord channel reader

---
+++ battleTabsLive.py
@@ -0,0 +1,39 @@
+# BattleTabs Live Bot
+# Keeps track of finished battles from the BattleTabs live discord channel
+
+from modules import config
+config = config.Config("config.json")
+
+import discord
+intents = discord.Intents.default()
+intents.guild_messages = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+from modules import runner
+rc = runner.RunnerClient(host="systems")
+
+@client.event
+async def on_message(message):
+    if message.channel.id != 779285528259330078: # #battletabs-live channel
+        return
+    # Process the message
+    embed = message.embeds[0]
+    if embed.title != "Battle Finished!":
+        return
+    for field in embed.fields:
+        if field.name != "Replay":
+            continue
+        # Process the replay field
+        replay_id = field.value[37].split("/")[0]
+        rc.event({ # send battle id off to the runner
+            "type": "process_battle",
+            "options": {
+                "replay_id": replay_id
+            }
+        })
+        return
+    return
+
+client.run(config.get("discord_token"))
